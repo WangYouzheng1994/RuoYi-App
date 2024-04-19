d51bf976f2cd49ebdc14b2af386b6edaeb66f6aa
wangyouzheng <353735348@qq.com>
2024-04-19 18:20:17 +0800

    大统 拣货 样式调整。

--- pages/picking/picking-work.vue
+++ pages/picking/picking-work.vue
@@ -33,9 +33,14 @@ export default {
           ckck: 'G2仓',
           bhstatus: '作业开始'
         },
-        {},{}
+        {}, {},{}
 
       ]
+    }
+  },
+  methods: {
+    test() {
+      alert("123")
     }
   }
 }
@@ -43,8 +48,9 @@ export default {
 
 <template>
   <view class="uni-title uni-common-mt">
-    <uni-collapse accordion>
-      <uni-collapse-item title="筛选条件">
+<!--    <view class="collapse-container">-->
+    <uni-collapse class="collapse" ref="collapse">
+      <uni-collapse-item title="筛选条件" >
         <view class="content">
           <!--        <text class="text">手风琴效果同时只会保留一个组件的打开状态，其余组件会自动关闭。</text>-->
           <uni-forms border label-width="85px">
@@ -55,6 +61,7 @@ export default {
               <uni-data-select
                   v-model="queryForm.warehouse"
                   :localdata="warehouseList"
+                  @click="test"
               ></uni-data-select>
             </uni-forms-item>
             <uni-forms-item label="备货单类型">
@@ -69,42 +76,34 @@ export default {
                   :localdata="pickingStatusList"
               ></uni-data-select>
             </uni-forms-item>
-
+            <uni-forms-item>
+            <button class="primary-button" size="mini" type="primary" style="border-radius: 20px;margin-right: 15px">检索</button>
+            <button class="primary-button" size="mini" type="primary" style="border-radius: 20px;">重置</button>
+            </uni-forms-item>
           </uni-forms>
         </view>
       </uni-collapse-item>
     </uni-collapse>
-
+<!--    </view>-->
     <uni-section title="备货清单" type="line" style="padding:0px 15px;">
       <uni-list>
-        <uni-list-item v-for="(item, index) in list" :key="index" :title="item.title" :note="item.note">
-          <!-- 右侧的图标和文字 -->
-<!--          <template v-slot:header>-->
-<!--            <view class="uni-list-icon">-->
-<!--              <uni-icons type="person" size="20"></uni-icons>-->
-<!--              <text>{{ item.author }}</text>-->
-<!--            </view>-->
-<!--          </template>-->
+        <uni-list-item class="uni-list-item" v-for="(item, index) in list" :key="index" :title="item.title" :note="item.note" >
+
+
           <!-- 内容的简介 -->
           <template v-slot:body>
-<!--            <view class="uni-list-text">{{ item.orderNumber }}</view>-->
-<!--            <uni-forms>
-              <uni-forms-item label="姓名">
-&lt;!&ndash;                <uni-easyinput placeholder="请输入姓名" />&ndash;&gt;
-                <view class="uni-list-text">{{ item.orderNumber }}</view>
-              </uni-forms-item>
-            </uni-forms>-->
-            <uni-card class="list-card" isFull="true">
+            <uni-card class="list-card ">
+              <uni-row class="uni-row" style="border-bottom: 1px solid rgb(238, 238, 238);margin-bottom: 15px;padding-bottom: 10px;">
+                <uni-col :span="12" style="display:flex;">
+                  <view class="label label-title">备货单号</view>
+                  <view class="label-value">123123</view>
+                </uni-col>
+                <uni-col :span="12" style="display:flex;justify-content: flex-end">
+                  <view class="label label-title">创建时间</view>
+                  <view class="label-value">123123</view>
+                </uni-col>
+              </uni-row>
               <uni-row class="uni-row">
-                <uni-col :span="24" style="display:flex;">
-                  <view class="label">备货单号</view>
-                  <view class="label-value">123123</view>
-                </uni-col>
-                <uni-col :span="24" style="display:flex;">
-                  <view class="label">备货单创建时间</view>
-                  <view class="label-value">123123</view>
-                </uni-col>
-
                 <uni-col :span="24" style="display:flex;">
                   <view class="label">出货仓</view>
                   <view class="label-value">G2仓库</view>
@@ -119,36 +118,14 @@ export default {
                   <view class="label">备货单状态</view>
                   <view class="label-value" style="color: crimson">作业新建</view>
                 </uni-col>
-
-                <uni-col :span="24" style="margin-top:15px;display:flex;">
-                  <button size="mini" type="primary" >开始备货</button>
+                <uni-col :span="24" style="margin-top:15px;display:flex; border-top: 1px solid rgb(238, 238, 238); padding-top: 15px;">
+                  <button class="primary-button" size="mini" type="primary" style="border-radius: 20px;width: 200px;">开始备货</button>
                 </uni-col>
               </uni-row>
             </uni-card>
-<!--
-            <uni-card title="基础卡片" :isFull="true" sub-title="副标题" extra="额外信息">
-              <text>这是一个通栏卡片 ，通栏没有外边距，左右会贴合父元素。</text>
-            </uni-card>-->
-
-
-            <!--            <view class="item">
-                          <view class="label">{{ item.orderNumber }}</view>
-                          <view class="label-value">123123</view>
-                        </view>
-                        <view class="item">
-                          <view class="label">{{ item.orderNumber }}</view>
-                          <view class="label-value">123123</view>
-                        </view>-->
-<!--            <view><button>asdf</button></view>-->
           </template>
-          <!-- 额外的图标 -->
-<!--          <template v-slot:footer>-->
-<!--            <view class="uni-list-icon">-->
-<!--              <button>确认备货</button>-->
-<!--            </view>-->
-<!--          </template>-->
         </uni-list-item>
-<!--        <uni-load-more iconType="circle" status="loding" />-->
+        <!--        <uni-load-more iconType="circle" status="loding" />-->
       </uni-list>
 
     </uni-section>
@@ -158,27 +135,73 @@ export default {
     </uni-list>-->
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .content {
   padding: 15px;
-  height: 300px;
+  height: 400px;
 }
 
 .list-card {
-  background-color: #95F204
+  //background-color: #1fbc97a1;
+  border-radius: 20px;
+  margin: 0px !important;
+  padding: 0px !important;;
 }
 
-.label,.label-value {
+.uni-list-item ::v-deep .uni-card--shadow {
+  box-shadow: rgb(66,80,191) 20px 20px 3px 1px !important;
+}
+
+.label, .label-value {
   font-size: 13px;
   padding-right: 15px;
   font-weight: bold;
   width: 125px;
   color: #606266;
 }
+.label-title {
+  font-size: 13.5px;
+}
+
 .label-value {
   font-weight: bold;
   color: black;
 }
 
+.primary-button {
+  background-color: rgb(66, 80, 191);
+}
+
+.uni-list-item ::v-deep .uni-list-item__container  {
+  padding: 0px 0px 0px 0px;
+  margin-bottom: 10px;
+}
+
+* {
+  font-family: "微软雅黑";
+}
+
+.bgcor-green {
+  background-image: linear-gradient(to bottom, #51e66d, #f7f7f7)
+}
+
+.bgcor-yellow {
+  background-image: linear-gradient(to bottom, #e0e651, #f7f7f7);
+}
+
+.bgcor-red {
+  background-image: linear-gradient(to bottom, #e68451, #f7f7f7);
+}
+
+// 测试粘性定位
+.collapse-container {
+  //height: 480px;
+  //overflow: auto;
+  top:0;
+}
+.collapse {
+  position: sticky;
+  top:20px;
+}
 
 </style>
